842a09f8dff16aa8be207324b297bb2ac392d2d3
briantical <lutbrianivan@gmail.com>
2024-06-17 20:54:27 +0300

    Setup linters

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Platform Running !!!");
+app.get('/', (req: Request, res: Response) => {
+	res.send('Platform Running !!!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
