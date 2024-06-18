6038b1ef54adedb67245604c37a19f49f57de560
briantical <lutbrianivan@gmail.com>
2024-06-18 16:47:40 +0300

    Fixing linters

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function App() {
-  return (
-    <div>
-      <h6>Welcome To The Platform</h6>
-    </div>
-  );
+	return (
+		<div>
+			<h6>Welcome To The Platform</h6>
+		</div>
+	);
 }
 
 export default App;
